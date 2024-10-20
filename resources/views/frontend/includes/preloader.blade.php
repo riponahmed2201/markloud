e5c254dd0b582064ceb25838ea54b1b41a9c1b2f
Md. Ripon Mia <33976306+riponahmed2201@users.noreply.github.com>
2024-10-20 13:24:07 +0600

    loader added

--- resources/views/frontend/includes/preloader.blade.php
+++ resources/views/frontend/includes/preloader.blade.php
@@ -3,11 +3,14 @@
         <div class="animation-preloader">
             <div class="spinner"></div>
             <div class="txt-loading">
-                <span class="letters-loading" data-text-preloader="E"> E </span>
-                <span class="letters-loading" data-text-preloader="N"> N </span>
-                <span class="letters-loading" data-text-preloader="P"> P </span>
-                <span class="letters-loading" data-text-preloader="I"> I </span>
-                <span class="letters-loading" data-text-preloader="X"> X </span>
+                <span class="letters-loading" data-text-preloader="M"> M </span>
+                <span class="letters-loading" data-text-preloader="A"> A </span>
+                <span class="letters-loading" data-text-preloader="R"> R </span>
+                <span class="letters-loading" data-text-preloader="K"> K </span>
+                <span class="letters-loading" data-text-preloader="L"> L </span>
+                <span class="letters-loading" data-text-preloader="O"> O </span>
+                <span class="letters-loading" data-text-preloader="U"> U </span>
+                <span class="letters-loading" data-text-preloader="D"> D </span>
             </div>
         </div>
     </div>
