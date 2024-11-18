d27a5dfff8a23e29e4b6f4301ee9384d213e99b2
Md. Ripon Mia <33976306+riponahmed2201@users.noreply.github.com>
2024-11-18 10:57:00 +0600

    home page service added

--- resources/views/frontend/pages/home/services-area.blade.php
+++ resources/views/frontend/pages/home/services-area.blade.php
@@ -13,7 +13,7 @@
         </div>
 
         <div class="relative">
-            <img class="circle-shape" src="assets/frontend/img/home/services/Group34.svg" alt="enpix">
+            <img class="circle-shape" src="assets/frontend/img/home/services/Group34.svg" alt="markloud">
             <img class="triangle-shape" src="assets/frontend/img/home/services/Trianglelineshape.svg" alt="enpix">
             <div class="row wow animate__animated animate__fadeInUp animate__delay-1s">
                 <div class="col-xl-3 col-sm-6">
@@ -32,7 +32,7 @@
                 </div>
                 <div class="col-xl-3 col-sm-6">
                     <div class="service-content">
-                        <img src="assets/frontend/img/home/services/001-design-tools1.svg" alt="enpix">
+                        <img src="assets/frontend/img/home/services/001-design-tools1.svg" alt="markloud">
                         <h3 class="heading-5"><a href="{{ route('service.graphicDesign') }}">Graphics Design</a></h3>
                         <p class="enpix-body-text-2">We design professional looking yet simple Logo.</p>
                         <a href="{{ route('service.graphicDesign') }}">
@@ -46,7 +46,7 @@
                 </div>
                 <div class="col-xl-3 col-sm-6">
                     <div class="service-content">
-                        <img src="assets/frontend/img/home/services/003-branding1.svg" alt="enpix">
+                        <img src="assets/frontend/img/home/services/003-branding1.svg" alt="markloud">
                         <h3 class="heading-5"><a href="{{ route('service.ppcEmailSmsMarketing') }}">Digital
                                 Marketing</a></h3>
                         <p class="enpix-body-text-2">We design professional looking yet simple Logo.</p>
@@ -61,11 +61,27 @@
                 </div>
                 <div class="col-xl-3 col-sm-6">
                     <div class="service-content">
-                        <img src="assets/frontend/img/home/services/004-pie-chart1.svg" alt="enpix">
+                        <img src="assets/frontend/img/home/services/004-pie-chart1.svg" alt="markloud">
                         <h3 class="heading-5"><a href="{{ route('service.ppcEmailSmsMarketing') }}">Business
                                 Consultancy</a></h3>
                         <p class="enpix-body-text-2">We design professional looking yet simple Logo.</p>
                         <a href="{{ route('service.ppcEmailSmsMarketing') }}">
+                            <svg width="37" height="15" viewbox="0 0 37 15" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path d="M30.1667 13.5098L36 7.50977L30.1667 1.50977M1 7.50977H36H1Z" stroke-width="1.5"
+                                    stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-25 wow animate__animated animate__fadeInUp animate__delay-1s">
+                <div class="col-xl-3 col-sm-6">
+                    <div class="service-content">
+                        <img src="assets/frontend/img/home/services/003-branding1.svg" alt="markloud">
+                        <h3 class="heading-5"><a href="{{ route('service.ppcEmailSmsMarketing') }}">PPC Ad, Email & SMS</a></h3>
+                        <p class="enpix-body-text-2">PPC Ads, Email, and SMS Marketing</p>
+                        <a href="{{ route('service.webDevelopment') }}">
                             <svg width="37" height="15" viewbox="0 0 37 15" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path d="M30.1667 13.5098L36 7.50977L30.1667 1.50977M1 7.50977H36H1Z" stroke-width="1.5"
